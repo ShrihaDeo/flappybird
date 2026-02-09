5eeade71f39ee9fc640b1e8232ba9d146356cbed
ShrihaDeo <shrihadeo009@gmail.com>
2026-02-09 15:30:04 +1300

    First bird movement

--- app.js
+++ app.js
@@ -28,30 +28,41 @@ function preload() {
   });
 }
 
-let bird;
+var bird;
+let hasLanded = false;
+let cursors;
 
 function create() {
   const background = this.add.image(0, 0, "background").setOrigin(0, 0);
+  const roads = this.physics.add.staticGroup();
+
   const topColumns = this.physics.add.staticGroup({
     key: "column",
     repeat: 1,
     setXY: { x: 200, y: 0, stepX: 300 },
-    });
+  });
 
   const bottomColumns = this.physics.add.staticGroup({
     key: "column",
     repeat: 1,
     setXY: { x: 350, y: 400, stepX: 300 },
-    });
+  });
 
-  const roads = this.physics.add.staticGroup();
-  const road = roads.create(400, 568, "road").setScale(2).refreshBody
+  const road = roads.create(400, 568, "road").setScale(2).refreshBody();
 
-  bird= this.physics.add.sprite(0, 50, 'bird').setScale(2);
+  bird = this.physics.add.sprite(0, 50, "bird").setScale(2);
   bird.setBounce(0.2);
   bird.setCollideWorldBounds(true);
+
+  this.physics.add.overlap(bird, road, () => (hasLanded = true), null, this);
   this.physics.add.collider(bird, road);
+
+  cursors = this.input.keyboard.createCursorKeys();
 
 }
 
-function update() {}
+function update() {
+  if (cursors.up.isDown && !hasLanded) {
+    bird.setVelocityY(-160);
+  }
+}
